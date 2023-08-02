16926d2c7e48b45f9af418d7fe9bc1b094121a8a
Suriya R <42648745+Ramsurya99@users.noreply.github.com>
2023-08-03 00:30:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Questions from "./components/QuizComponent";
+import Questions from "./QuizComponent";
 
 const App = () =>{
     return (
